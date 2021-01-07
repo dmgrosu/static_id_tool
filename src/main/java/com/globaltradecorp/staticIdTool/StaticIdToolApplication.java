406ad2ca19e1b548b4f83986e3e6583e6719c13b
Dmitri Grosu <dmitri.grosu@codefactorygroup.com>
2021-01-07 14:29:35 +0200

    initial commit

---
+++ src/main/java/com/globaltradecorp/staticIdTool/StaticIdToolApplication.java
@@ -0,0 +1,13 @@
+package com.globaltradecorp.staticIdTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticIdToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticIdToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globaltradecorp/staticIdTool/StaticIdToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaltradecorp.staticIdTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticIdToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
